07c644075b1a453345bfc8295f894c8dcf3ec4b7
syzer3@gmail.com <syzer3@gmail.com>
2016-01-12 13:22:09 +0100

    dot env

---
+++ config.js
@@ -0,0 +1,7 @@
+require('dotenv');
+export const config =  {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    dbName: process.env.DB_NAME
+};
---
+++ repo.js
@@ -0,0 +1,9 @@
+import Sequelize from 'sequelize';
+import Faker from 'faker';
+import _ from 'lodash';
+
+export function repository(aker = ) {
+    return {
+        
+    }
+}
