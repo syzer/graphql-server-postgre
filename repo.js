159167524b85a23e59c2dfeb53e04b41a050cb56
syzer3@gmail.com <syzer3@gmail.com>
2016-01-12 13:59:48 +0100

    repo.. first try

--- repo.js
+++ repo.js
@@ -2,8 +2,18 @@ import Sequelize from 'sequelize';
 import Faker from 'faker';
 import _ from 'lodash';
 
-export function repository(aker = ) {
+export function repository(config) {
+    const connect = new Sequelize(
+        config.dbName,
+        'postgres',
+        'postgres',
+        {
+            dialect: 'postgres',
+            host: config.host
+        }
+    );
+
     return {
-        
+        connect
     }
 }
