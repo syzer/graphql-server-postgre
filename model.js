a53e237956bbe59ee4b3060a7ad822d7e0e776cf
syzer3@gmail.com <syzer3@gmail.com>
2016-01-14 14:18:06 +0100

    db models

---
+++ model.js
@@ -0,0 +1,42 @@
+module.exports = function model(Conn, Sequelize) {
+    return {
+        getArticleSchema,
+        getAuthorSchema
+    };
+
+    function getArticleSchema() {
+        return Conn.define('article', {
+            title: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            abstract: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            content: {
+                type: Sequelize.STRING,
+                allowNull: false
+            }
+        });
+    }
+
+    function getAuthorSchema() {
+        return Conn.define('author', {
+            firstName: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            lastName: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            email: {
+                type: Sequelize.STRING,
+                validate: {
+                    isEmail: true
+                }
+            }
+        });
+    }
+};
